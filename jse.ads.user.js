83a4b4ed173aacc7e4d8cb73fa25dd5e07556ccf
SlayTheVirus <alexanderfrantom@gmail.com>
2019-09-12 12:09:14 -0400

    Update jse.ads.user.js

--- jse.ads.user.js
+++ jse.ads.user.js
@@ -4,7 +4,7 @@
 // @version      0.1
 // @description  try to take over the world!
 // @author       You
-// @match        https://*/
+// @match        https://*/*
 // @grant        none
 // ==/UserScript==
 
