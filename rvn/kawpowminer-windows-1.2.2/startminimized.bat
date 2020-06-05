e1113fd104257e16129969453bacde1498ee5aae
SlayTheVirus <slaythevirus@gmail.com>
2020-06-05 05:41:52 -0400

    unzip

---
+++ rvn/kawpowminer-windows-1.2.2/startminimized.bat
@@ -0,0 +1,7 @@
+:start
+echo off
+taskkill /IM kawpowminer.exe
+timeout 10
+start /min kawpowminer.exe -P stratum+tcp://RF6TtDF2d1coFCp8iHRsSPe2fb3LfSRZs4.miner1/alexanderfrantom@gmail.com@rvn-us-east1.nanopool.org:12222
+timeout 600
+goto start
